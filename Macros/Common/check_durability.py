5b1ad8d516076574804b6be4bfcfdfc80b830cb6
Denis Savchuk <denis.savchuk@vineti.com>
2020-06-23 21:30:29 +0400

    feat: add some usefull macroses

---
+++ Macros/Common/check_durability.py
@@ -0,0 +1,65 @@
+# Name: Durability check
+# Description: Checks all items for durability and show alerts
+# Author: Mordor
+# Era: Any
+
+from Assistant import Engine
+
+layers = [
+    'OneHanded',
+    'TwoHanded',
+    'Shoes',
+    'Pants',
+    'Shirt',
+    'Helm',
+    'Gloves',
+    'Ring',
+    'Talisman',
+    'Neck',
+    'Waist',
+    'InnerTorso',
+    'Bracelet',
+    'MiddleTorso',
+    'Earrings',
+    'Arms',
+    'Cloak',
+    'OuterTorso',
+    'OuterLegs',
+    'InnerLegs',
+]
+
+# Amount of durability to alert
+minDurability = 20
+# Checks every 5 secs
+checkDelay = 1000 * 60  # every 1 min
+
+
+def property_exists(serial, cliloc):
+    item = Engine.Items.GetItem(serial)
+
+    if (item == None or item.Properties == None):
+        return False
+
+    for x in item.Properties:
+        if x.Cliloc == cliloc:
+            return True
+
+    return False
+
+
+def check_durability():
+    while True:
+        while not Dead('self'):
+            for layer in layers:
+                if FindLayer(layer) and property_exists(GetAlias('found'), 'durability'):
+                    durability = PropertyValue[int]('found', 'durability')
+                    Pause(500)
+
+                    if durability != None and durability < minDurability:
+                        HeadMsg("ATTENTION!! \"" + layer + "\": " +
+                                str(durability), 'self')
+
+                Pause(checkDelay)
+
+
+check_durability()
---
+++ Macros/Skills/Peacemaking/train_peacemaking.py
@@ -0,0 +1,92 @@
+# Name: Peacemaking training
+# Description: Uses the Peacemaking skill on the player to train Peacemaking to GM
+# Author: Mordor
+# Era: Any
+
+# Script variables configuration
+peacemaking_timer_milliseconds = 10000
+journal_entry_delay_milliseconds = 1000
+skill_cap = SkillCap('Peacemaking')
+
+
+def train_peacemaking():
+    '''
+    Trains Peacemaking to GM
+    '''
+    # Script variables
+    global peacemaking_timer_milliseconds
+    global journal_entry_delay_milliseconds
+    global skill_cap
+
+    if not ListExists('instruments'):
+        CreateList('instruments')
+        PushList('instruments', 0x2805)  # bamboo flute
+        PushList('instruments', 0x0e9c)  # drum
+        PushList('instruments', 0x0eb3)  # lute
+        PushList('instruments', 0xeb2)  # lap harp
+        PushList('instruments', 0x0eb1)  # standing harp
+        PushList('instruments', 0x0e9e)  # tambourine
+        PushList('instruments', 0x0e9d)  # tambourine (tassle)
+
+    if Skill('Peacemaking') == skill_cap:
+        MessageBox("Done", 'You\'ve already maxed out Peacemaking!')
+        return
+
+    # Initialize skill timers
+    SetTimer('peacemaking_timer', peacemaking_timer_milliseconds)
+
+    # Initialize the journal
+    ClearJournal()
+
+    while not Dead('self') and Skill('Peacemaking') < skill_cap:
+        if Timer('peacemaking_timer') >= peacemaking_timer_milliseconds:
+            # Clean skill timers and jornal
+            SetTimer('peacemaking_timer', 0)
+            ClearJournal()
+
+            UseSkill('Peacemaking')
+
+            Pause(journal_entry_delay_milliseconds)
+
+            # Handle the Journal response
+            if InJournal('What instrument shall you play?'):
+                for i in GetList("instruments"):
+                    if FindType(i, -1, "backpack"):
+                        break
+
+                if not FindAlias('found'):
+                    MessageBox("Error", "No instrument to peacemake with.")
+                    return
+
+                WaitForTarget(1000)
+                Target('found')
+
+            WaitForTarget(1000)
+            Target('self')
+
+            # Wait for the journal entry to come up
+            while Timer('peacemaking_timer') < peacemaking_timer_milliseconds:
+
+                if (InJournal('You play hypnotic music, calming your target.', 'regular') or
+                    InJournal('You play your hypnotic music, stopping the battle.', 'regular') or
+                    InJournal('You attempt to calm everyone, but fail.', 'regular') or
+                    InJournal('You play hypnotic music, but there is nothing in range for you to calm.', 'regular') or
+                    InJournal('You attempt to calm your target, but fail.', 'regular') or
+                    InJournal('You have no chance of calming that creature', 'regular') or
+                    InJournal(
+                    'You play poorly, and there is no effect.', 'regular')
+                    ):
+                    # Skill was used successfully, even if the enemy was not successfully put to peace
+                    SetTimer('peacemaking_timer',
+                             peacemaking_timer_milliseconds)
+
+                Pause(journal_entry_delay_milliseconds)
+
+            ClearJournal()
+
+        # Wait a little bit so that the while loop doesn't consume as much CPU
+        Pause(50)
+
+
+# Start Peacemaking training
+train_peacemaking()
