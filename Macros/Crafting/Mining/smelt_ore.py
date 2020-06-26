3b7331994d06c92b0581545fe34a3af4db46ad57
johnscott78 <67349199+johnscott78@users.noreply.github.com>
2020-06-26 22:13:12 +0800

    Create smelt_ore.py

---
+++ Macros/Crafting/Mining/smelt_ore.py
@@ -0,0 +1,33 @@
+# Name: Smelt Ore
+# Description: Smelt ore from a container and put ingots on ground back into container
+# Author: Reetus
+# Era: Any
+
+from Assistant import Engine
+
+if not FindObject('ore chest'):
+	PromptAlias('ore chest')
+
+if not FindObject('forge'):
+	PromptAlias('forge')
+
+oreTypes = [0x19b7, 0x19b8, 0x19b9, 0x19ba]
+
+ClearIgnoreList()
+
+for ore in oreTypes:
+	while FindType(ore, -1, 'ore chest'):
+		if Graphic('found') == 0x19b7:
+			item = Engine.Items.GetItem(GetAlias('found'))
+			if item.Count <= 1:
+				IgnoreObject('found')
+				continue
+		UseObject('found')
+		WaitForTarget(5000)
+		Target('forge')
+		Pause(1000)
+		#IgnoreObject('found')
+		while FindType(0x1bf2, 3):
+			MoveItem('found', 'ore chest')
+			Pause(1000)
+			IgnoreObject('found')
