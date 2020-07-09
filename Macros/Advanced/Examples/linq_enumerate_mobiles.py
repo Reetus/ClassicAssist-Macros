1cec66d788b149da8f6f63247d5a14f49f26c43f
Reetus <6239195+Reetus@users.noreply.github.com>
2020-07-10 06:29:32 +0800

    Update linq_enumerate_mobiles.py

--- Macros/Advanced/Examples/linq_enumerate_mobiles.py
+++ Macros/Advanced/Examples/linq_enumerate_mobiles.py
@@ -1,5 +1,5 @@
 # Name: Linq Enumerate Mobiles
-# Description: Using Linq with Python to cycle through mobiles with a bunch of conditions...
+# Description: Using Linq with Python to get mobiles with a bunch of conditions...
 # Author: Reetus
 # Era: Any
 
@@ -9,17 +9,48 @@ clr.AddReference("System.Core")
 clr.ImportExtensions(System.Linq)
 from Assistant import Engine
 
-mounts = [0x115, 0xb3]
+# Possible notorieties
+# Innocent, Ally, Attackable, Criminal, Enemy, Murderer, Invulnerable
 
-mount = Engine.Mobiles.Where(lambda m: mounts.Contains(m.ID) 
-                                        and m.Distance < 10 
-					and not InFriendList(m.Serial) 
-					and not InIgnoreList(m.Serial)).FirstOrDefault()
+humans = [0x190, 0x191]
 
-if (mount == None):
-	ClearIgnoreList()
-	SysMessage('No mount found')
-	Stop()
+def GetMobiles(ids = None, notorieties = None, includeFriends = False, includeIgnored = False, maxDistance = 32, orderBy = lambda m: m.Distance):
+	mobiles = Engine.Mobiles.Where(lambda m: (ids == None or ids.Contains(m.ID))
+                                             	and m.Distance < maxDistance
+                                             	and m.Serial != Engine.Player.Serial
+                                             	and (notorieties == None or notorieties.Contains(m.Notoriety.ToString()))
+						and (includeFriends or not InFriendList(m.Serial))
+						and (includeIgnored or not InIgnoreList(m.Serial))).OrderBy(orderBy)
+	return mobiles
 
-SetEnemy(mount.Serial)
-IgnoreObject(mount.Serial)
+# Get 1 
+mobile = GetMobiles(ids = humans, notorieties = ['Murderer'], maxDistance = 10).First()
+HeadMsg(mobile.Name, 'self')
+
+# Get 2
+mobiles = GetMobiles(ids = humans, notorieties = ['Murderer', 'Innocent'], maxDistance = 10, includeFriends = True).Take(2)
+
+for m in mobiles:
+	HeadMsg(m.Name, 'self')
+
+# Order by Hits
+mobile = GetMobiles(orderBy = lambda m: m.Hits, notorieties = ['Innocent']).First()
+HeadMsg(mobile.Name + ' - ' + str(mobile.Hits), 'self')
+
+# Order by Hits, ThenBy alphabetical
+mobile = GetMobiles(orderBy = lambda m: m.Hits).ThenBy(lambda m: m.Name).First()
+HeadMsg(mobile.Name + ' - ' + str(mobile.Hits), 'self')
+
+# Get all less than 5, and select just the serials
+mobiles = GetMobiles(maxDistance = 5, includeFriends = True).Select(lambda m: m.Serial)
+
+for m in mobiles:
+	HeadMsg(hex(m), 'self')
+
+# Only friends
+mobiles = GetMobiles(maxDistance = 15, includeFriends = True).Where(lambda m: InFriendList(m.Serial))
+
+for m in mobiles:
+	HeadMsg(m.Name, 'self')
+
+
