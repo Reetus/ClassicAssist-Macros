3e569a3fe528bd1706f7bfe12afc8f2d787f5d1e
ziox-b <50050722+ziox-b@users.noreply.github.com>
2020-11-29 08:20:30 +0100

    Update HUO-Lumberjacker.py

--- Macros/Crafting/HUO-Lumberjacker.py
+++ Macros/Crafting/HUO-Lumberjacker.py
@@ -1,7 +1,7 @@
 # Name: Lumberjacker 
-# Description: Use 2 runic atlas with 48 runes on each, 1 hatchet, full LRC suit
-# Author: ziox-b (inspired from "Cray" script)
-# Era: UO Heritage
+# Description: Use runic atlas (3 or 4) with 48 runes on each, 1 hatchet, full LRC suit
+# Author: ziox-b (inspired by "Cray" script)
+# Shard: UO Heritage
 
 from Assistant import Engine
 from ClassicAssist.UO.Commands import UO3DEquipItems
@@ -13,8 +13,7 @@ axes = [0x0f43,0x1443]
 logs = [0x1bdd,0x07da,0x04a7,0x04a8,0x04a9,0x04aa,0x047f] # colors
 to_cont = [0x1bdd,0x1bd7,0x1bd7,0x2f5f,0x318f,0x3190,0x3191,0x3199,0x5738] # dump
 cont_serial = 0x40159b2d # add your container serial to drop stuff
-runebook1 = 0x400b631d # home rune must be the first one in this runic atlas
-runebook2 = 0x4003a799 
+runic_atlas = [0x400b631d, 0x4003a799] # home rune must be the first one in first serial
 runes = range(1, 48)
 # end settings
 
@@ -54,7 +53,7 @@ def	drop_to_home():
 	if Weight() <= MaxWeight() - 200:
 		Pause(100)
 		return
-	recall(runebook1,0)
+	recall(runic_atlas[0],0)
 	for items in to_cont:
 		while FindType(items, -1, 'backpack'):
 			MoveItem('found', cont_serial)
@@ -85,14 +84,10 @@ def lumber(runebook):
 		Pause(1000)
 
 while not Dead('self'):
-	for r in runes:
-		cut_logs()
-		drop_to_home()
-		recall(runebook1, r)
-		lumber(runebook1)
-	for r in runes:
-		cut_logs()
-		drop_to_home()
-		recall(runebook2, r)
-		lumber(runebook2)
-	Pause(100)
+    for books in runic_atlas:
+        for r in runes:
+            cut_logs()
+            drop_to_home()
+            recall(books, r)
+            lumber(books)
+    Pause(100)
