272cb4ea198062c21bfc7bd16f0f39d9c7f481a2
Tad Taylor <bittiez@gmail.com>
2022-07-05 18:35:32 -0600

    Create makelastcount.py

---
+++ Macros/Skills Crafting/makelastcount.py
@@ -0,0 +1,13 @@
+# Name: Make Last Item
+# Description: Asks you how many items you would like to make using make last
+# Author: Bittiez
+# Shard: Ruins and Riches
+
+res, amt = MessagePrompt("How many times would you like to make last item?", "50")
+
+if res:
+    i = 0
+    while i < amt:
+        ReplyGump(0x38920abd, 21)
+        WaitForGump(0x38920abd, 10000)
+        i += 1
