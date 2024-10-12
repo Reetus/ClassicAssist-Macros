b569630dfdd047252326d8b6542a79b9dd452bac
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2024-10-12 16:29:19 -0300

    Better naming and comments

--- Macros/Resource Gathering/CollectDeedsLuna.py
+++ Macros/Resource Gathering/CollectDeedsLuna.py
@@ -19,7 +19,7 @@ WaitForGump(0x9bade6ea, 2000)
 ReplyGump(0x9bade6ea, 1)
 Pause(500)
 
-#Finds and set alias for each BOD
+#Finds and set alias for each BOD book
 while FindType(BODBook, -1, "backpack"):
     WaitForProperties("found", 3000)
     if Property("found", "BS"):
@@ -29,9 +29,11 @@ while FindType(BODBook, -1, "backpack"):
         SetAlias("Tailor_BODBook", "found")
         IgnoreObject("found")
     Pause(700)
+#Find and move each Tailor Deed into the Tailor Book
 while FindType(TailorDeed[0], -1, "backpack", TailorDeed[1]):
     MoveItem("found", "Tailor_BODBook")
     Pause(700)
+#Find and move each BS Deed into the Tailor BS
 while FindType(BSDeed[0], -1, "backpack", BSDeed[1]):
     MoveItem("found", "BS_BODBook")
     Pause(700)
